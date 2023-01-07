489a8adaee3c1334c2344b78d3b169a682f56df7
Oscar2019 <omadureira2000@gmail.com>
2023-01-07 11:22:22 -0300

    overlay finished; fixed FPS update

--- src/StageState.cpp
+++ src/StageState.cpp
@@ -3,7 +3,6 @@
 #include "../header/Sound.h"
 #include "../header/Vec2.h"
 #include "../header/TileSet.h"
-#include "../header/StaticBox.h"
 #include "../header/TileMap.h"
 #include "../header/InputManager.h"
 #include "../header/Camera.h"
@@ -15,6 +14,7 @@
 #include "../header/Text.h"
 #include "../header/Player.h"
 #include "../header/TestBox.h"
+#include "../header/SpriteRect.h"
 
 StageState::StageState() : State()
 {
@@ -61,6 +61,7 @@ void StageState::LoadAssets()
     // Camera
     Camera::Follow(playerGo);
 
+    // @TODO Tests START here ---
     // Box
     GameObject* testBoxGo = new GameObject();
     TestBox* testBox = new TestBox(*testBoxGo);
@@ -79,19 +80,39 @@ void StageState::LoadAssets()
     testBoxGo2->AddComponent(testBox2);
     AddObject(testBoxGo2);
 
-    // Static Box 1
+    // SpriteRect 2
+    GameObject* staticBoxGo2 = new GameObject();
+    SpriteRect* spriteRect2 = new SpriteRect(*staticBoxGo2, 0xFF8888FF, 100, 100);
+    CameraFollower* cameraFollower2 = new CameraFollower(*staticBoxGo2, Vec2(250, 250));
+    
+    staticBoxGo2->AddComponent(spriteRect2);
+    staticBoxGo2->AddComponent(cameraFollower2);
+    AddObject(staticBoxGo2, 1);
+
+    // SpriteRect 3
+    GameObject* staticBoxGo3 = new GameObject();
+    SpriteRect* spriteRect3 = new SpriteRect(*staticBoxGo3, 0x5555FF55, 100, 100);
+    CameraFollower* cameraFollower3 = new CameraFollower(*staticBoxGo3, Vec2(225, 225));
+    
+    staticBoxGo3->AddComponent(spriteRect3);
+    staticBoxGo3->AddComponent(cameraFollower3);
+    AddObject(staticBoxGo3, 2);
+
+    // SpriteRect 1
     GameObject* staticBoxGo = new GameObject();
-    StaticBox* staticBox = new StaticBox(*staticBoxGo);
+    SpriteRect* spriteRect = new SpriteRect(*staticBoxGo, 0x88888888, 100, 100);
+    CameraFollower* cameraFollower = new CameraFollower(*staticBoxGo, Vec2(200, 200));
     
-    staticBoxGo->box.SetVec(Vec2(200, 200));
-    
-    staticBoxGo->AddComponent(staticBox);
-    AddObject(staticBoxGo);
+    staticBoxGo->AddComponent(spriteRect);
+    staticBoxGo->AddComponent(cameraFollower);
+    AddObject(staticBoxGo, 3);
+
+    // @TODO Tests END here ---
 
     // FPS counter
-    GameObject* textGo = new GameObject();
-    CameraFollower* textFollower = new CameraFollower(*textGo, textGo->box.GetVec());
-    textGo->AddComponent(textFollower);
+    fpsCounter = new GameObject();
+    CameraFollower* textFollower = new CameraFollower(*fpsCounter, fpsCounter->box.GetVec());
+    fpsCounter->AddComponent(textFollower);
 
     const char* fontFile = "./assets/font/call_me_maybe.ttf";
     const char* textStr = "FPS ";
@@ -99,10 +120,10 @@ void StageState::LoadAssets()
     Text::TextStyle style = Text::BLENDED;
     SDL_Color color = {212, 15, 15, 255};
     
-    Text* text = new Text(*textGo, fontFile, fontSize, style, textStr, color);
-    textGo->AddComponent(text);
+    Text* text = new Text(*fpsCounter, fontFile, fontSize, style, textStr, color);
+    fpsCounter->AddComponent(text);
     
-    AddObject(textGo);
+    AddObject(fpsCounter);
 }
 
 void StageState::Update(float dt)
@@ -132,36 +153,53 @@ void StageState::Update(float dt)
             if (objectArray[i][j]->IsDead())
                 objectArray[i].erase(objectArray[i].begin() + j);
 
-            // Updates FPS counter - Turning into a component might be better 
-            Text* FPS_Text = (Text*) objectArray[i][j]->GetComponent("Text");
-            if (FPS_Text != nullptr)
-                FPS_Text->SetText(("FPS " + to_string(floor(GameData::currentFPS))).c_str());
-
             // Checks for colisions
             else
             {
-                for (unsigned long j = i + 1; j < objectArray.size(); j++)
-                for (unsigned long j = i + 1; j < objectArray.size(); j++)
+                uint32_t iniK = 0;
+                uint32_t iniL = 0;
+                
+                if (j + 1 == objectArray[i].size())
                 {
-                    Collider* colliderA = (Collider*) objectArray[i]->GetComponent("Collider");
-                    Collider* colliderB = (Collider*) objectArray[j]->GetComponent("Collider");
-                    if (colliderA != nullptr && colliderB != nullptr)
+                    iniK = i + 1;
+                    iniL = 0;
+                } 
+                
+                else
+                {
+                    iniK = i;
+                    iniL = j + 1;
+                }
+                
+                for (uint32_t k = iniK; k < objectArray.size(); k++)
+                {
+                    for (uint32_t l = iniL; l < objectArray[k].size(); l++)
                     {
-                        if (Collision::IsColliding(colliderA->box, colliderB->box, objectArray[i]->angleDeg, objectArray[j]->angleDeg))
+                        Collider* colliderA = (Collider*) objectArray[i][j]->GetComponent("Collider");
+                        Collider* colliderB = (Collider*) objectArray[k][l]->GetComponent("Collider");
+                        if (colliderA != nullptr && colliderB != nullptr)
                         {
-                            objectArray[i]->NotifyCollision(*objectArray[j]);
-                            objectArray[j]->NotifyCollision(*objectArray[i]);
-                            Collision::ResolveCollision(*colliderA, *colliderB);
-                            
-                            // Update collisions before rendering
-                            colliderA->ResolveCollisionUpdate(dt);
-                            colliderB->ResolveCollisionUpdate(dt);
+                            if (Collision::IsColliding(colliderA->box, colliderB->box, objectArray[i][j]->angleDeg, objectArray[k][l]->angleDeg))
+                            {
+                                objectArray[i][j]->NotifyCollision(*objectArray[k][l]);
+                                objectArray[k][l]->NotifyCollision(*objectArray[i][j]);
+                                Collision::ResolveCollision(*colliderA, *colliderB);
+                                
+                                // Update collisions before rendering
+                                colliderA->ResolveCollisionUpdate(dt);
+                                colliderB->ResolveCollisionUpdate(dt);
+                            }
                         }
                     }
                 }
             }
         }
     }
+
+    // Updates FPS counter
+    Text* FPS_Text = (Text*) fpsCounter->GetComponent("Text");
+    if (FPS_Text != nullptr)
+        FPS_Text->SetText(("FPS " + to_string(floor(GameData::currentFPS))).c_str());
 }
 
 void StageState::Render()
