eaed84f9b37d2cf0e91e98e4240a2cbcb0af32ff
gustavo-tomas <guga.tomas10@gmail.com>
2023-01-28 20:35:00 -0300

    Adiciona lógica de vitória/derrota

--- src/Player.cpp
+++ src/Player.cpp
@@ -131,5 +131,7 @@ bool Player::Is(const char* type)
 
 void Player::NotifyCollision(GameObject& other)
 {
-    
+    Bullet* bullet = (Bullet *) other.GetComponent("Bullet");
+    if (bullet != nullptr)
+        hp -= bullet->GetDamage();
 }
