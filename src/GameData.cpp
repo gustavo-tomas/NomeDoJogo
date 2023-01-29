47c42e8ec5022359d4e0dba537d209e5692b86a9
gustavo-tomas <guga.tomas10@gmail.com>
2023-01-29 11:32:01 -0300

    Adiciona caminho para pasta world

--- src/GameData.cpp
+++ src/GameData.cpp
@@ -9,3 +9,5 @@ Vec2 GameData::playerPos = Vec2(0, 0);
 float GameData::targetFPS = 60.0f;
 float GameData::currentFPS = 0.0f;
 float GameData::delay = 1000.0f / targetFPS;
+
+const string GameData::objectsPath = "./assets/image/world/";
